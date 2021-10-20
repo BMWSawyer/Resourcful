ff3b6f36eb76e8478e36ebcd5fc59ed1a11ce957
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-10-20 21:37:52 +0000

    Create comments table

---
+++ db/schema/03_comments.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS comments CASCADE;
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  comment TEXT
+);
