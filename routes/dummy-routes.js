5b77e177ea586d36ed76e58133e1b901e1bfed6d
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-10-23 18:26:54 +0000

    update resources ejs to use new sample input objects

--- routes/dummy-routes.js
+++ routes/dummy-routes.js
@@ -17,34 +17,60 @@ module.exports = () => {
 
   router.get("/profile", (req, res) => {
     res.render('profile', {
-      error:"test issue",
-      user:{id:"1", firstName:"Test", lastName:"McTester", email:"mctester@fakeemail.com"}
+      error: "test issue",
+      user: { id: "1", firstName: "Test", lastName: "McTester", email: "mctester@fakeemail.com" }
     });
   });
 
   router.get("/resources/:id", (req, res) => {
     res.render('resources', {
-      error:"test issue",
-
-      resource:{title:"Sample Title",
-      id: "2",
-      imgURL: "/mockups/mockupResources/sharon-mccutcheon-tn57JI3CewI-unsplash.jpg",
-      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      URL: "http://www.amazon.ca",
-      topic: "Fast food",
-      ownerID: "2"},
-
-      user:{id:"2", firstName:"Test", lastName:"McTester", email:"mctester@fakeemail.com"},
-
-      resource_ratings:{
-        id: "1",
-        user_id: "1",
-        resource_id: "2",
-        rating: "2",
-        liked: true
+      user: {
+        id: 1,
+        firstName: "Test",
+        lastName: "McTester",
+        email: "mctester@fakeemail.com"
+      },
+      resource: {
+        id: 1,
+        creatorId: 1,
+        title: "More Stuff",
+        description: "read all this",
+        resourceUrl: "http://...",
+        photoUrl: "http://...",
+        resourceRating: {
+          liked: true,
+          rating: 4,
+        },
+        averageRating: 3.4,
+        comments: [{
+          id: 1,
+          firstName: "Isabelle",
+          lastName: "Ringing",
+          comment: "This was a great resource!",
+          date: "2019-09-07T15:50+00Z"
+        },
+        ],
       },
 
-      ratingsTotal:3.2
+      // resource:{title:"Sample Title",
+      // id: "2",
+      // imgURL: "/mockups/mockupResources/sharon-mccutcheon-tn57JI3CewI-unsplash.jpg",
+      // description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      // URL: "http://www.amazon.ca",
+      // topic: "Fast food",
+      // ownerID: "2"},
+
+      // user:{id:"2", firstName:"Test", lastName:"McTester", email:"mctester@fakeemail.com"},
+
+      // resource_ratings:{
+      //   id: "1",
+      //   user_id: "1",
+      //   resource_id: "2",
+      //   rating: "2",
+      //   liked: true
+      // },
+
+      // ratingsTotal:3.2
     });
   });
 
