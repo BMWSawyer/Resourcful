ee1b859613b1f3b4529fbebdcc12cfb6b1271e4a
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-10-23 01:47:58 +0000

    Adding initial logic to new route files

---
+++ routes/comment.js
@@ -0,0 +1,26 @@
+/*
+ * All routes for comments are defined here
+ */
+
+const express = require('express');
+const router  = express.Router();
+const { getUserWithEmail } = require('../database');
+
+module.exports = (db) => {
+
+  router.post('/', (req, res) => {
+    const {email, password} = req.body;
+    login(email, password)
+      .then(user => {
+        if (!user) {
+          res.send({error: "error"});
+          return;
+        }
+        req.session.user_id = user.id;
+        res.render("/my-resources", resources)
+      })
+      .catch(error => res.send(error));
+  });
+
+  return router;
+};
