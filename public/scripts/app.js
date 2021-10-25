b99fc031f03d5065d01c5f2e0e8a40cadeeb6665
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-10-25 02:32:45 +0000

    add noembed api request functionality to successfully receive objects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,29 @@
 // Client facing scripts here
-const moment = require('moment');
+
+//Ensure page is loaded first. Add event handlers in here.
+$(function () {
+
+
+  const getNoEmbed = function (url) {
+    let noEmbedUrl = "http://noembed.com/embed?url=";
+    if (url) {
+      noEmbedUrl += url;
+    }
+    return $.ajax({
+      url: noEmbedUrl,
+    });
+  };
+
+  //Add new resource button functionality
+  $('#urlSubmit').on('click', function () {
+    const urlInput = $('#addResURLInput').val();
+    console.log(urlInput);
+
+    getNoEmbed(urlInput)
+      .then((res) => {
+        console.log(res);
+      });
+  });
+
+
+});
