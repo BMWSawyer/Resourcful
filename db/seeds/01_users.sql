1d675e7601a68c6d3448b7164ed29aab6e522575
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-10-25 01:58:28 +0000

    Updated seeds data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (first_name, last_name, email, password)
+VALUES
+('Anna', 'Bell', 'anna@belle.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('John' , 'Snow', 'john.snow@got.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
