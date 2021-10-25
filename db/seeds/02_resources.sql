b6d6baf4b65f89a48a6016a480d1cf7923e6d896
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-10-25 01:58:49 +0000

    Created new seeds data for resources

---
+++ db/seeds/02_resources.sql
@@ -0,0 +1,4 @@
+INSERT INTO resources (creator_id, title, description, resource_url, photo_url)
+VALUES
+(1, 'YouTube', 'Video sharing platform', 'https://www.youtube.com/', 'https://unsplash.com/photos/niUkImZcSP8'),
+(2, 'Google', 'Search engine', 'https://www.google.com/', 'https://unsplash.com/photos/jz4ca36oJ_M');
