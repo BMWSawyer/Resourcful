859897ea82dbe897cf9e24f446db12039524f369
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-10-26 01:32:00 +0000

    update profile route to populate page

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,13 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const bcrypt = require('bcrypt');
 const { addUser, getUserWithId, getUserWithEmail, updateUser } = require('../database');
+
+const camelCase = (str) => {
+  return str.replace(/(_\w)/g, (k) => k[1].toUpperCase());
+};
 
 module.exports = (db) => {
 
@@ -16,23 +20,23 @@ module.exports = (db) => {
    * @param {String} email
    * @param {String} password encrypted
   **/
-   const login =  function(email, password) {
+  const login = function (email, password) {
     return getUserWithEmail(email, db)
-    .then(user => {
-      if (bcrypt.compareSync(password, user.password)) {
-        return user;
-      }
-      return null;
-    });
+      .then(user => {
+        if (bcrypt.compareSync(password, user.password)) {
+          return user;
+        }
+        return null;
+      });
   }
 
   router.post('/login', (req, res) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     login(email, password)
       .then(user => {
         if (!user) {
-          res.send({error: "error"});
+          res.send({ error: "error" });
           return;
         }
 
@@ -60,14 +64,14 @@ module.exports = (db) => {
 
     user.password = bcrypt.hashSync(user.password, 12);
     addUser(user, db)
-    .then(user => {
-      if (!user) {
-        res.send({error: "error"});
-        return;
-      }
-      res.redirect("/");
-    })
-    .catch(error => res.send(error));
+      .then(user => {
+        if (!user) {
+          res.send({ error: "error" });
+          return;
+        }
+        res.redirect("/");
+      })
+      .catch(error => res.send(error));
   });
 
 
@@ -76,14 +80,20 @@ module.exports = (db) => {
     const userId = req.session.user_id;
 
     getUserWithId(userId, db)
-    .then(user => {
-      if (!user) {
-        res.send({error: "error"});
-        return;
-      }
-      res.render("profile", {user: user});
-    })
-    .catch(error => res.send(error));
+      .then(user => {
+        if (!user) {
+          res.send({ error: "error" });
+          return;
+        }
+
+        //Convert object keys to camelcase
+        user = Object.fromEntries(
+          Object.entries(user)
+            .map(i => [camelCase(i[0]), i[1]]));
+
+        res.render("profile", { user });
+      })
+      .catch(error => res.send(error));
   });
 
   router.post('/profile/update', (req, res) => {
@@ -101,7 +111,7 @@ module.exports = (db) => {
     updateUser(userData, userId, db)
       .then(user => {
         if (!user) {
-          res.send({error: "error"});
+          res.send({ error: "error" });
           return;
         }
         res.send(user)
